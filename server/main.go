c80a4b5c32bdae576a2a32861b0bd756368e4581
John Jones <john@johnjonesfour.com>
2022-09-21 12:38:33 -0400

    cleanup

--- server/main.go
+++ server/main.go
@@ -1,15 +1,8 @@
 package main
 
 import (
-	"bytes"
-	"encoding/json"
 	"fmt"
-	"io"
-	"net/http"
-	"os"
 	"time"
-
-	"github.com/google/uuid"
 )
 
 func main() {
@@ -23,46 +16,9 @@ func main() {
 		if err != nil {
 			fmt.Println(err)
 		} else if down != lastStatus || lastStatusTime.Before(time.Now().Add(-24*time.Hour)) {
-			err := sendAlert(os.Getenv("UPSTREAM_URL"), down)
-			if err != nil {
-				fmt.Println(err)
-			} else {
-				lastStatusTime = time.Now()
-			}
+			fmt.Printf("Station is up: %t\n", !down)
 		}
 		lastStatus = down
 		time.Sleep(time.Minute * 60)
 	}
-}
-
-func sendAlert(host string, down bool) error {
-	if host == "" {
-		return nil
-	}
-	payloadMap := map[string]interface{}{
-		"eventVendorType": "weather-station",
-		"eventVendorID":   uuid.NewString(),
-		"vendorInfo": map[string]interface{}{
-			"up": !down,
-		},
-		"alerts":   []interface{}{},
-		"isNormal": !down,
-		"created":  time.Now().UTC(),
-	}
-
-	payloadBytes, err := json.Marshal(payloadMap)
-	if err != nil {
-		return err
-	}
-
-	resp, err := http.Post(fmt.Sprintf("http://%s/api/event", host), "application/json", io.NopCloser(bytes.NewBuffer(payloadBytes)))
-	if err != nil {
-		return err
-	}
-
-	if resp.StatusCode != http.StatusOK {
-		return fmt.Errorf("http error: %d/%s", resp.StatusCode, resp.Status)
-	}
-
-	return nil
 }
